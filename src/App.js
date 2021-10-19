b52ab3100d99db8846cdd23a73cab5e0552a4b52
wakenedo <a_nuner@outlook.com>
2021-10-19 15:06:33 -0300

    Athentication
    App:
    Express & Cors (installed)
    Refactory of App.js
    
    Login:
    Refactory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
+import './App.css';
+import Login from './components/Login';
 
+function setToken(userToken) {
+  
+}
+function getToken() {
+
+}
 
 function App() {
+  const token = getToken();
+
+  if(!token) {
+    return <Login setToken={setToken}/>;
+  }
+
   return (
     <div className='wrapper'>
       <h1>Application</h1>
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
 
-function Login() {
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+}
+
+export default function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
     return (
     <div className="login-wrapper">
         <h1>Please Log In</h1>
         <form>
             <label>
                 <p>Username</p>
-                <input type='text' />
+                <input type='text' onChange={e => setUserName(e.target.value)} />
             </label>
             <label>
                 <p>Password</p>
-                <input type='password' />
+                <input type='password' onChange={e => setPassword(e.target.value)} />
             </label>
             <div>
                 <button type='submit'>Submit</button>
@@ -21,4 +44,7 @@ function Login() {
     )
 }
 
-export default Login
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+
